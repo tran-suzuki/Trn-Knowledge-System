6de105a51846c1d7bfd4aa1fb28671043816d132
hieubv <hieuvietbui@gmail.com>
2026-01-05 16:23:28 +0700

    #20250105: mt_usersテーブルにname_kanaカラムを追加するマイグレーションを追加

---
+++ src/database/migrations/2026_01_05_091722_add_name_kana_to_mt_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mt_users', function (Blueprint $table) {
+            Schema::table('mt_users', function (Blueprint $table) {
+                $table->string('name_kana', 255)
+                    ->after('name');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mt_users', function (Blueprint $table) {
+            Schema::table('mt_users', function (Blueprint $table) {
+                $table->dropColumn('name_kana');
+            });
+        });
+    }
+};
